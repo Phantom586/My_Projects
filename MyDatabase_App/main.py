dbf28a11cee86e627b1afc80c6c5b4d5dfedb544
Phantom Boy <586pboy@gmail.com>
2019-05-10 00:48:27 +0530

    commit for major changes in main.py

--- MyDatabase_App/main.py
+++ MyDatabase_App/main.py
@@ -4,7 +4,7 @@ from tkinter import *
 # importing tkinter as themed-tkinter module
 import tkinter.ttk as ttk
 
-from tkinter import messagebox, scrolledtext
+from tkinter import messagebox, scrolledtext, PhotoImage
 
 # importing my Custom-sqlite-Database File
 from MyDatabase import Database
@@ -17,9 +17,9 @@ my_db = Database('records_bank')
 win = tk.Tk()
 
 # Setting the Window Size
-win.geometry('390x550')
-win.minsize(390, 550)
-win.maxsize(390, 550)
+win.geometry('390x580')
+win.minsize(390, 580)
+win.maxsize(390, 580)
 
 # Configuring the Style for the themed-tkinter module
 style = ttk.Style()
@@ -33,20 +33,33 @@ win.title("My Database App")
 fname_var = tk.StringVar()    # Variable for Firstname
 lname_var = tk.StringVar()    # Variable for Lastname
 uid_var = tk.StringVar()      # Variable for User-Id
+uid = ''
 pas_var = tk.StringVar()      # Variable for User-Password
+pas = ''
 email_var = tk.StringVar()    # Variable for User-Email
-text_widget = tk.scrolledtext.ScrolledText(win, width=12, height=4)     # Text-Widget Variable
-text_widget_1 = tk.scrolledtext.ScrolledText(win, width=12, height=6)   # Another Text-Widget Variable
+pinentry_var = tk.StringVar() # Variable for the Password-verify Button
 
 
 # ---------------------- Buttons ---------------------------
 
-preview_btn = ''      # Button for Preview Function
-register_btn = ''     # Button for Register Function
-signin_btn = ''       # Button for SignIn Function
-search_btn = ''       # Button for Search Function
+preview_btn = ''          # Button for Preview Function
+register_btn = ''         # Button for Register Function
+signin_btn = ''           # Button for SignIn Function
+search_btn = ''           # Button for Search Function
+statusbtn_var = tk.StringVar() # Text Variable for status_btn
+status = "LOCKED"
 
 SUNKABLE_BUTTON = 'SunkableButton.TButton'  # Style for themed-tkiner module
+
+# ----------------------- Path for Icons -------------------
+
+lock_img = "icons/lock1.png"
+unlock_img = "icons/unlock1.png"
+
+# ----------------------- PhotoImage Object for the Button --
+
+image = lock_img
+img = PhotoImage(file=image)
 
 
 # ----------------------- Functions -------------------------
@@ -57,7 +70,6 @@ def remove_all_widgets():
     Description:
         Function to remove all the widgets from the window.
     """
-    global win
     for widget in win.winfo_children():
         widget.grid_remove()
 
@@ -125,7 +137,7 @@ def add_user(fname, lname, u_id, u_pass, email):
         # Issuing the Roor Message if not.
         messagebox.showerror('Error!!', 'Blank Fields are not Allowed!!')
     else:
-        # Retrieving the Details of all the Users and checking if the User Doesn't exist already. 
+        # Retrieving the user_id and user_pass of all the Users and checking if the User exist already. 
         check = my_db.select_from_db('account_details', ['user_id', 'user_pass'])
         # print(check)
         for entries in check:
@@ -163,10 +175,11 @@ def validate_user(u_id, u_pass):
     # Just making Sure all the inputs are Valid.
     if u_id != '' and u_pass != '':
         # Retrieving the Details of all the Users and checking if it exists.
-        d = my_db.select_from_db('account_details', user_id=u_id, user_pass=u_pass)
-        if d:
+        user = my_db.select_from_db('account_details', user_id=u_id, user_pass=u_pass)
+        if user:
             print("User Exists!!!")
-                
+            remove_all_widgets()
+            create_user_screen(user)
         else:
             # If User Doesn't Exists Issuing a Proper Warning.
             print("User Doesn't Exists!!!")
@@ -191,12 +204,13 @@ def query_db():
     print(c)
 
 
-def preview(u_id, u_pass):
+def preview(u_id, u_pass, msg=None):
 
     """
     Parameters:
         u_id = the 'Id' of the User to be Previewed.
         u_pass = the 'Password' of the User to be Previewed.
+        msg = for custom messages.
 
     Description:
         Function to just make sure that all the Details filled by the User is Correct.
@@ -207,7 +221,10 @@ def preview(u_id, u_pass):
         result = my_db.select_from_db('account_details', user_id=u_id, user_pass=u_pass)
         # print(result)
         # Passing the Values along with the message to be displayed in the Output Window.
-        output_win(result, "Preview Results : ")
+        if msg:
+            output_win(result, msg)
+        else:
+            output_win(result, "Preview Results : ")
     else:
         messagebox.showerror('Error!!', 'Blank Fields are not Allowed!!')
 
@@ -245,6 +262,27 @@ def search(u_fname, u_id):
     output_win(result, "Search Results :")
 
 
+def extract_info(user):
+    """
+    Parameters:
+        u_id = the 'Id' of the User to be Previewed.
+        u_pass = the 'Password' of the User to be Previewed.
+
+    Description:
+        to extract all different fields from the specific row of a table.
+    """
+
+    global fname_var, lname_var, uid_var, pas_var, email_var
+    # print(user)
+    user = list(user[0])
+    # print(user)
+    fname_var.set(user[0])
+    lname_var.set(user[1])
+    uid_var.set(user[2])
+    pas_var.set(user[3])
+    email_var.set(user[4])
+
+
 def output_win(str, msg):
 
     """
@@ -280,6 +318,99 @@ def shortcuts(param):
         win.destroy()
 
 
+def pass_verify(u_pass):
+    """
+    Parameters:
+        u_pass = the 'user_pass' of the user.
+
+    Description:
+        to verify if the user is Authentic or not.
+    """
+
+    global top, status, pinentry_var
+
+    if status ==  "LOCKED":
+
+        top = tk.Toplevel(win)
+        top.title("Verify!!")
+
+        style.configure(SUNKABLE_BUTTON, foreground='green')
+
+        top.geometry('220x100')
+        top.minsize(220, 100)
+        top.maxsize(220, 100)
+
+        lb1 = tk.Label(top, text="Enter the Password for the User :")
+        lb1.grid(row=0, columnspan=4, padx=20, sticky="news")
+
+        u_p = tk.Entry(top, textvariable=pinentry_var)
+        u_p.grid(row=1, columnspan=4, padx=20, sticky="news", pady=10)
+
+        u_p.focus_set()
+
+        btn = ttk.Button(top, text="Verify", style=SUNKABLE_BUTTON)
+        btn.bind("<Button-1>", lambda x: btn_status(u_p.get(), u_pass))
+        btn.grid(row=2, column=1, columnspan=2, sticky="news", pady=10)
+
+        top.mainloop()
+
+    else:
+        
+        btn_status()
+
+
+
+def btn_status(t_pass=None, u_pass=None):
+    """
+    Parameters:
+        t_pass = the 'pass' entered by the user in verification process.
+        u_pass = the 'user_pass' of the user to verify during state change.
+
+    Description:
+        to check whether the button is in LOCKED state or not.
+    """
+    global statusbtn_var, fname, lname, email, status, top, pinentry_var, img, status_btn
+
+    # Checking if the Passwords Exixts or not.
+    if t_pass and u_pass:
+        if t_pass == u_pass:
+            # If the Passwords Match then,
+            
+            # Create a PhotoImage Object to change the Icon of the Button.
+            img = PhotoImage(file=unlock_img)
+            # Setting that icon to the Button
+            status_btn.config(image=img)
+            # Clearing the Pin Entry.
+            pinentry_var.set('')
+            # Destroying the Pin Verification Window.
+            top.destroy()
+            # Changing the Buttons Text to UNLOCKED.
+            statusbtn_var.set("UNLOCKED")
+            # Making the Entries Editable.
+            fname.config(state=NORMAL)
+            lname.config(state=NORMAL)
+            email.config(state=NORMAL)
+            # Setting the Global Variable to UNLOCKED.
+            status = "UNLOCKED"
+        else:
+            # if not then Display the Error Message and Clear the Pin Entry.
+            messagebox.showerror('Authentication Error!!!', "Please Try Again!!")
+            pinentry_var.set('')
+    else:
+        # If the Status is UNLOCKED then set the button's text to LOCKED.
+        statusbtn_var.set("LOCKED")
+        # Create a PhotoImage Object to change the Icon of the Button.
+        img = PhotoImage(file=lock_img)
+        # Setting that icon to the Button
+        status_btn.config(image=img)
+        # Making the Entries Non-Editable.
+        fname.config(state=DISABLED)
+        lname.config(state=DISABLED)
+        email.config(state=DISABLED)
+        status = "LOCKED"
+
+
+
 # ---------------------- Key Shortcuts ------------------
 win.bind("<Control-q>", lambda e:shortcuts("quit"))
 
@@ -308,8 +439,11 @@ def create_login_screen():
         Function to create the login screen.
     """
 
-    global fname_var, lname_var, uid_var, pas_var, email_var, search_btn, preview_btn, register_btn, signin_btn, SUNKABLE_BUTTON
-    global text_widget_1
+    global uid, pas
+
+    # Text-Widgets for Displaying the Info and Results.
+    text_widget = tk.scrolledtext.ScrolledText(win, width=12, height=6)     # Text-Widget Variable
+    text_widget_1 = tk.scrolledtext.ScrolledText(win, width=12, height=6)   # Another Text-Widget Variable
 
     # The Title of the Window.
     lb1 = tk.Label(win, text="Database App")
@@ -359,7 +493,7 @@ def create_login_screen():
     # --------------------------------------------------------
 
     # ----------------- Information Text Block ---------------
-    text_widget.insert('insert', """USER-ID and PASSWORD are unique for everyone!!\nFor SignIn Only USER-ID and PASSWORD Fields are required!!""")
+    text_widget.insert('insert', """USER-ID and PASSWORD are unique for everyone!!\nFor SignIn Only USER-ID and PASSWORD Fields are required!!\nTo Search Somebody Input his USER-ID and PASSWORD.""")
     text_widget.grid(row=6, columnspan=2, padx=10, pady=20, sticky="news")
     text_widget.config(state="disabled")
 
@@ -371,7 +505,7 @@ def create_login_screen():
     style.configure(SUNKABLE_BUTTON, foreground='green')
 
     preview_btn = ttk.Button(frame_btns, text="Preview", style=SUNKABLE_BUTTON)
-    preview_btn.bind("<Button-1>", lambda x: preview(uid.get(), pas.get()))
+    preview_btn.bind("<Button-1>", lambda x: preview(uid.get(), pas.get(), '1'))
     preview_btn.grid(row=0, column=0)
 
     register_btn = ttk.Button(frame_btns, text="Register", style=SUNKABLE_BUTTON)
@@ -385,6 +519,10 @@ def create_login_screen():
     search_btn = ttk.Button(frame_btns, text="Search", style=SUNKABLE_BUTTON)
     search_btn.bind("<Button-1>", lambda x: search(fname.get(), uid.get()))
     search_btn.grid(row=0, column=3)
+
+    clear_btn = ttk.Button(frame_btns, text="Clear", style=SUNKABLE_BUTTON)
+    clear_btn.bind("<Button-1>", lambda x: clear_pin_entries(''))
+    clear_btn.grid(row=0, column=4)
 
     # --------------- The Output Text Block -----------------------
 
@@ -401,16 +539,90 @@ def create_login_screen():
     lb8.grid(row=10, columnspan=2, pady=20)
 
 
-def create_user_screen():
+def create_user_screen(user):
 
     """
+    Parameters:
+        user = details of the user whose account is being accessed.
     Description:
         Function for the User to provide functionality of Update, View and Delete Account.
     """
+    global uid, pas, statusbtn_var, fname, lname, email, status, status_btn
+
+    # Text-Widgets for Displaying the Info and Results.
+    text_widget = tk.scrolledtext.ScrolledText(win, width=12, height=6)     # Text-Widget Variable
     
     lb1 = tk.Label(win, text="Database App")
     lb1.config(font=("Source Code Pro", 28))
-    lb1.grid(row=0, columnspan=4, ipadx=160, ipady=20)
+    lb1.grid(row=0, columnspan=4, ipadx=60, ipady=20)
+
+    lb2 = tk.Label(win, text="------------ Your Details -----------")
+    lb2.grid(row=1, columnspan=4, sticky="news")
+
+    extract_info(user)
+
+    # ------------- Form Labels and Inputs --------------
+
+    lb2 = tk.Label(win, text="FIRSTNAME : ")
+    lb2.config(font=("Source Code Pro", 10))
+    lb2.grid(row=2, column=0, sticky="e")
+
+    fname = tk.Entry(win, textvariable=fname_var, state=DISABLED)
+    fname.grid(row=2, column=1, pady=2, ipadx=70, sticky="nws")
+
+    lb3 = tk.Label(win, text="LASTNAME : ")
+    lb3.config(font=("Source Code Pro", 10))
+    lb3.grid(row=3, column=0, sticky="e")
+
+    lname = tk.Entry(win, textvariable=lname_var, state=DISABLED)
+    lname.grid(row=3, column=1, pady=2, ipadx=70, sticky="nws")
+
+    lb4 = tk.Label(win, text="USER-ID : ")
+    lb4.config(font=("Source Code Pro", 10))
+    lb4.grid(row=4, column=0, sticky="e")
+
+    uid = tk.Entry(win, textvariable=uid_var, state=DISABLED)
+    uid.grid(row=4, column=1, pady=2, ipadx=70, sticky="nws")
+
+    lb5 = tk.Label(win, text="PASSWORD : ")
+    lb5.config(font=("Source Code Pro", 10))
+    lb5.grid(row=5, column=0, sticky="e")
+
+    pas = tk.Entry(win, textvariable=pas_var, state=DISABLED)
+    pas.config(show='*')
+    pas.grid(row=5, column=1, pady=2, ipadx=70, sticky="nws")
+
+    lb6 = tk.Label(win, text="EMAIL : ")
+    lb6.config(font=("Source Code Pro", 10))
+    lb6.grid(row=6, column=0, sticky="e")
+
+    email = tk.Entry(win, textvariable=email_var, state=DISABLED)
+    email.grid(row=6, column=1, pady=2, ipadx=70, sticky="nws")
+
+    # --------------------------------------------------------
+
+    # ----------------- Information Text Block ---------------
+    text_widget.insert('insert', """To Make Changes to any of the Input Fields(Except USER-ID and PASSWORD) just Press the LOCKED button and then type in the new Entries and then Press UPDATE button!!""")
+    text_widget.grid(row=7, columnspan=2, padx=10, pady=20, sticky="news")
+    text_widget.config(state="disabled")
+
+    # ----------------- Buttons --------------------
+
+    frame_btns = tk.Frame(win)
+    frame_btns.grid(row=8, columnspan=3)
+
+    style.configure(SUNKABLE_BUTTON, foreground='green')
+
+    statusbtn_var.set("LOCKED")
+
+    status_btn = ttk.Button(frame_btns, textvariable=statusbtn_var, style=SUNKABLE_BUTTON, compound="left")
+    status_btn.config(image=img)
+    status_btn.bind("<Button-1>", lambda x: pass_verify(pas.get()))
+    status_btn.grid(row=0, column=0)
+
+    update_btn = ttk.Button(frame_btns, text="Update", style=SUNKABLE_BUTTON)
+    update_btn.bind("<Button-1>", lambda x: update_user(fname.get(), lname.get(), uid.get(), pas.get(), email.get()))
+    update_btn.grid(row=0, column=1)
 
 
 
